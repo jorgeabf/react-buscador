40d244eed102dad521485759927508407fb8e0b5
Jorge <aportuvas@outlook.es>
2022-05-16 12:05:20 +0200

    Cambio título

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import "./App.css";
 import Buscador from "./components/Buscador";
 
 function App() {
   return (
     <div className="container-fluid bg-light">
-      <h1 className="text-center py-4">Buscador con React</h1>
+      <h1 className="text-center py-4">
+        Buscador con React Vercel
+      </h1>
       <Buscador />
     </div>
   );
